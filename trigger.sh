de8e620813e8920f390a8b01fb82cdf2f29a1317
Tom George <tom@penberth.com>
2019-02-15 08:15:36 +0000

    Added first influxdb call

--- trigger.sh
+++ trigger.sh
@@ -2,3 +2,4 @@
 /usr/bin/curl http://192.168.1.154/control?cmd=GPIO,4,1
 sleep 1
 /usr/bin/curl http://192.168.1.154/control?cmd=GPIO,4,0
+/usr/bin/curl -i -XPOST 'http://192.168.1.16:8086/write?db=collectdb' --data-binary 'gate_opened value=1'
